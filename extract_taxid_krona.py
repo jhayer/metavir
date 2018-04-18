2d99447dbd584f350c4a24af715996205e1dfefc
RVanDamme <38455046+RVanDamme@users.noreply.github.com>
2018-04-18 15:36:09 +0200

    Adding the use of more than 1 branch name
    We can now submit a list of branch name, coma-delimited
    example: -b "Alphacoronavirus,Betacoronavirus"

--- extract_taxid_krona.py
+++ extract_taxid_krona.py
@@ -39,7 +39,7 @@ def extract_names(branch_name, krona_file, name_file):
 	taxa_names = sorted(set(taxa_names))
 	file_r.close()	
 	if name_file:
-		file_w=open(name_file,"w")
+		file_w=open(name_file,"a")
 		for tax_n in taxa_names:			
 			file_w.write("%s\n"%tax_n)
 		file_w.close()
@@ -49,13 +49,13 @@ def extract_names(branch_name, krona_file, name_file):
 def extract_id (taxa_names, out_file):
 	raw_taxa = []
 	taxa = []
-	Entrez.email = 'renaud.van.damme@slu.se'
+	Entrez.email = 'EMAIL_ADRESS'
 	for name in taxa_names:
 		id_handle = Entrez.esearch(db="Taxonomy",term=name,idtype="taxid")
 		id_rec = Entrez.read(id_handle)
 		raw_taxa.append(id_rec['IdList'])
 	taxa = [s.strip("[']") for stri in raw_taxa for s in stri]
-	file_w=open(out_file,"w")
+	file_w=open(out_file,"a")
 	for tax_id in taxa:
 		file_w.write("%s,"%tax_id)
 	file_w.close()
@@ -65,20 +65,22 @@ def main():
 
     parser = argparse.ArgumentParser(description="")
 
-    parser.add_argument("-b", "--branch_name", help="Name of the targeted branch of the krona tree")
+    parser.add_argument("-b", "--branch_name", help="One or more names (coma-delemited) of Branches in the .krona file ")
     parser.add_argument("-f", "--krona_file", help="Krona file to retrieve Taxid from")
     parser.add_argument("-o", "--out_file", help="Output name for the list of ids; Default: ids_extracted.out")
     parser.add_argument("-n", "--name_file", help="Output name for the list of names; Default: None")
 
     args = parser.parse_args()
 
-    branch_name = args.branch_name
+    branches = args.branch_name
     krona_file = args.krona_file
     out_file = args.out_file
     name_file = args.name_file
-    out_file = checker(branch_name, krona_file, out_file)
-    taxa_names = extract_names(branch_name, krona_file, name_file)
-    extract_id(taxa_names, out_file)
+    
+    for branch_name in branches.split(','):
+	    out_file = checker(branch_name, krona_file, out_file)
+	    taxa_names = extract_names(branch_name, krona_file, name_file)
+	    extract_id(taxa_names, out_file)
 
 if __name__ == '__main__':
     main()
